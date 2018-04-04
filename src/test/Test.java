9d425952e211f5496b05419770cc63f8f191e136
hp <hp@shurooqGhaith>
2018-04-04 03:38:25 -0700

    first

---
+++ src/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Test {
+
+	public static int fun(int x,int y ){
+		if(x>y)return 1;
+		else return 0;
+	}
+	
+	public static void main(String args[]){
+		System.out.println(fun(2,3));
+		System.out.println(fun(3,3));
+		
+	}
+}
